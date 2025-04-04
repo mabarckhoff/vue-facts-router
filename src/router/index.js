4d7a7b9230fbfebbad732d8ef42769aa552d88c0
mabarckhoff <145296130+mabarckhoff@users.noreply.github.com>
2025-04-03 20:22:47 -0400

    Changing to createWebHashHistory() routing for use on GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
@@ -38,7 +38,9 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  
+  //history: createWebHistory(process.env.BASE_URL), // Use for local
+  history: createWebHashHistory('/vue-facts-router/'), // Use for GitHub
   routes,
 });
 
